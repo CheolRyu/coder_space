8156b10b314979bf4cc2adf1c06c6c6a7ff0ff5e
charlesworld1994@gmail.com <charlesryu@Charless-MacBook-Pro.local>
2019-06-23 14:00:01 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My project</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
